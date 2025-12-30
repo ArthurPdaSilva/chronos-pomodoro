4e5b512f9144e06b811232c0e53740ebb48e98a7
Arthur Pereira da Silva <72280602+ArthurPdaSilva@users.noreply.github.com>
2025-12-30 15:54:19 -0300

    style: format code and improve consistency across files

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,12 @@
-import { StrictMode } from 'react';
-import { createRoot } from 'react-dom/client';
-import { App } from './App.tsx';
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App.tsx";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+// biome-ignore lint/style/noNonNullAssertion: false positive
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<App />
+	</StrictMode>,
 );
 
 // O StrictMode é um recurso do React que ajuda a identificar problemas potenciais no aplicativo durante o desenvolvimento. Ele ativa verificações adicionais e avisos para componentes filhos, mas não afeta o comportamento do aplicativo em produção. (Causa o double render no dev mode)
